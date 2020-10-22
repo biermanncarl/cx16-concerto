4bc361931b54edacf9940cf13d6442ae506733a3
biermanncarl <carlgeorg@gmx.net>
2020-10-23 00:14:26 +0200

    added rudimentary AD envelope

--- psg_synth.asm
+++ psg_synth.asm
@@ -34,16 +34,32 @@ Octave:
 Note:
    .byte 0
 
+   ; AD-Envelope variables
+   ; front end
+AD_attack:
+   .byte 0
+AD_decay:
+   .byte 0
+   ; back-end
+AD_attack_rate:
+   .word 0
+AD_decay_rate:
+   .word 0
+AD_step:
+   .byte 0
+AD_phase:
+   .word 0
+
 
    ; handles the sound generation
 My_isr:
    ; first check if interrupt is an AFLOW interrupt
    lda VERA_isr
    and #$08
-   bne @do_fillup
-   jmp @end_aflow
+   bne do_fillup
+   jmp end_aflow
 
-@do_fillup:
+do_fillup:
    ; fill FIFO buffer with 4 samples
    ; this will generate the next AFLOW interrupt in ~10 ms
    ; at lowest possible PCM playback rate
@@ -55,9 +71,63 @@ My_isr:
    sta VERA_audio_data
 
    ; now do all PSG control
-@do_psg_control:
+do_psg_control:
+   ; do env generator
+   ; advance first
+   ; then update volume
+   lda AD_step
+   cmp #1
+   beq AD_do_attack
+   cmp #2
+   beq AD_do_decay
+   bra end_aflow
+AD_do_attack:
+   clc
+   lda AD_attack_rate
+   adc AD_phase
+   sta AD_phase
+   lda AD_attack_rate+1
+   adc AD_phase+1
+   sta AD_phase+1
+   tax
+   cmp #63
+   bcc AD_update_volume ; if 64 is still larger than high byte, just update volume
+   inc AD_step
+   lda #63
+   tax
+   sta AD_phase+1
+   stz AD_phase
+   bra AD_update_volume
+AD_do_decay:
+   sec
+   lda AD_phase
+   sbc AD_decay_rate
+   sta AD_phase
+   lda AD_phase+1
+   sbc AD_decay_rate+1
+   bcc AD_finished
+   sta AD_phase+1
+   tax
+   bra AD_update_volume
+AD_finished:
+   lda #0
+   tax
+   stz AD_step
+AD_update_volume:
+   lda #0
+   sta VERA_ctrl
+   lda #$C2
+   sta VERA_addr_low
+   lda #$F9
+   sta VERA_addr_high
+   lda #$11
+   sta VERA_addr_bank
+   txa
+   clc
+   adc #192    ; activate channels LR
+   sta VERA_data0
 
-@end_aflow:
+end_aflow:
    ; call default interrupt handler
    ; for keyboard service
    jmp (Default_isr)
@@ -95,6 +165,17 @@ start:
    sta IRQVec+1
    cli            ; allow interrupts
 
+   ; initialize AD env generator
+   ; for now, just use the rates directly
+   ; instead of deriving them from times
+   lda #0
+   sta AD_attack_rate
+   lda #63
+   sta AD_attack_rate+1
+   lda #0
+   sta AD_decay_rate
+   lda #5
+   sta AD_decay_rate+1
 
    ; start playback of PSG waveform
    ; select port 0 to the VERA
@@ -326,6 +407,11 @@ mainloop:
    lda pitch_data,x
    sta $9F23   ; freq high byte
 
+   ; launch ENV generator
+   stz AD_phase
+   stz AD_phase+1
+   lda #1
+   sta AD_step
 @end_mainloop:
 
    jmp mainloop
